94d5788c609d684f9e0dacdf922e556d81221d1c
vikash <vickykumar89990@gmail.com>
2023-07-23 06:39:56 +0530

    added_cart

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Header from "./components/Layout/Header";
 import { Fragment } from "react";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart></Cart>
       <Header/>
       <main>
         <Meals/>
---
+++ src/components/UI/Modal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classes from "./Modal.module.css";
+import { Fragment } from "react";
+import ReactDOM from "react-dom";
+
+const Backdrop = () => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverLay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+const portalElement = document.getElementById("overlay");
+
+const Modal = (props) => {
+  return (
+    <Fragment>
+      {ReactDOM.createPortal(<Backdrop />, portalElement)}
+      {ReactDOM.createPortal(
+        <ModalOverLay>{props.children}</ModalOverLay>,
+        portalElement
+      )}
+    </Fragment>
+  );
+};
+
+export default Modal;
